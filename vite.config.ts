eef4b5c1c88730ad1dfbc8c3e391c6694f66deaa
[dyad] <git@dyad.sh>
2026-05-28 11:32:06 +0530

    [dyad] wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
-  plugins: [dyadComponentTagger(), react(), require('@capacitor/vite-plugin').capacitor({})],
+  plugins: [dyadComponentTagger(), react()], // Capacitor plugin removed
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
